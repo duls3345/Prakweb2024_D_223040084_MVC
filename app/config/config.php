2ec6fca6e28619152a74e653b54336abe2ba4c60
duls3345 <azis.rohman33@gmail.com>
2024-10-30 19:46:52 +0700

    Database Wrepper

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('BASEURL', 'http://localhost/Pw/mvc/public/');
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2024_d_223040084_mvc');
