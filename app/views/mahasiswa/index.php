e9d5fd9714e0ece3986edef37c5efe6779ee65cd
duls3345 <azis.rohman33@gmail.com>
2024-11-03 03:04:30 +0700

    Update Data (+ ajax)

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -6,24 +6,26 @@
     </div>
     <div class="row">
         <div class="col-lg-6">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+            <button type="button" class="btn btn-primary tombolTambahData
+                " data-toggle="modal" data-target="#formModal">
                 Tambah Data Mahasiswa
             </button>
             <br><br>
             <h3>Daftar Mahasiswa</h3>
-            <?php foreach ($data['mhs'] as $mhs) : ?>
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
                 <li class="list-group-item">
                     <?= $mhs['nama']; ?>
                     <a href="<?= BASEURL; ?>mahasiswa/hapus/<?= $mhs['id']; ?>"
                         class="badge badge-danger float-right ml-1" onclick="return confirm('yakin?');">Hapus</a>
+                    <a href="<?= BASEURL; ?>mahasiswa/ubah/<?= $mhs['id']; ?>"
+                        class="badge badge-success float-right ml-1 tampilModalUbah" data-id="<?= $mhs['id'] ?>"
+                        data-toggle="modal" data-target="#formModal">Ubah</a>
                     <a href="<?= BASEURL; ?>mahasiswa/detail/<?= $mhs['id']; ?>"
                         class="badge badge-primary float-right ml-1">Detail</a>
                 </li>
                 <?php endforeach; ?>
             </ul>
-            <?php endforeach; ?>
         </div>
     </div>
 </div>
@@ -39,6 +41,7 @@
             </div>
             <div class="modal-body">
                 <form action="<?= BASEURL; ?>mahasiswa/tambah" method="post">
+                    <input type="hidden" name="id" id="id">
                     <div class="form-group">
                         <label for="nama">Nama</label>
                         <input type="text" class="form-control" id="nama" name="nama">
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -7,7 +7,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
     integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
 </script>
-<script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="<?= BASEURL; ?>js/bootstrap.js"></script>
+<script src="<?= BASEURL; ?>js/script.js"></script>
 </body>
 
 </html>
